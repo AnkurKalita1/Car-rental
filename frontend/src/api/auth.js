f59b249a2ab7e73d0d07775769a49fee53e1d240
AnkurKalita1 <ankurkalita455@gmail.com>
2026-02-08 11:21:30 +0530

    auth.js change

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,11 +1,11 @@
 import client from "./client";
 
 export const register = async (payload) => {
-  const { data } = await client.post("/auth/register", payload);
+  const { data } = await client.post("/api/auth/register", payload);
   return data;
 };
 
 export const login = async (payload) => {
-  const { data } = await client.post("/auth/login", payload);
+  const { data } = await client.post("/api/auth/login", payload);
   return data;
 };
